aa3769d24fa738e0deb44e0df36ec421a3b42564
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-02 20:05:31 +0300

    Update push.cpp

--- examples/push.cpp
+++ examples/push.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackPush() {
+	cout << endl << "---push.cpp---" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++) { arrayInt.push(i*2); }
+	for(size_t j = 0; j < arrayInt.count(); j++) { cout << arrayInt[j] << " "; }
+	cout << endl;
+}
 
 int main() {
 	stackPush();
