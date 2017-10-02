89f7d2aa9fc08810bfdc6afc8e6e2f60b5c6989d
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-02 20:04:45 +0300

    Update init.cpp

--- examples/init.cpp
+++ examples/init.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackInit() {
+	cout << endl << "---init.cpp---" << endl;
+	Stack<int> arrayInt;
+	cout << "arrayInt created" << endl;
+}
 
 int main() {
 	stackInit();
