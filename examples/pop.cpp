2200b1a3aea5fbbc45e243cfd5741b3b5c2a9f81
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-02 20:05:09 +0300

    Update pop.cpp

--- examples/pop.cpp
+++ examples/pop.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackPop() {
+	cout << endl << "---pop.cpp---" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++) { arrayInt.push(i); }
+	for(int j = 0; j < 6; j++) { cout << arrayInt.pop() << " "; }
+	cout << endl << endl;
+}
 
 int main() {
 	stackPop();
