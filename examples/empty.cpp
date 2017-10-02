afab00726cda4ba91d73d738ab873719f1b230b3
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-02 20:04:19 +0300

    Update empty.cpp

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackEmpty() {
+	cout << endl << "---empty.cpp---" << endl;
+	Stack<char> arrayChar;
+	char wordChar[] = "KCATS_RAHC";
+	for(int i = 0; i < strlen(wordChar); i++) { arrayChar.push(wordChar[i]); }
+	while(!arrayChar.empty()) {
+		cout << arrayChar.pop() << " ";
+	}
+	cout << endl << endl;
+}
 
 int main() {
 	stackEmpty();
