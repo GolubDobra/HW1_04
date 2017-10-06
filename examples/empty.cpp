24d3288be584f752bc4d37ff1c053673f10fa4b1
VladislavMashkov <Vladislav_mashkow@mail.ru>
2017-10-07 00:52:43 +0300

    update files

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 void stackEmpty() {
-	cout << endl << "---empty.cpp---" << endl;
-	Stack<char> arrayChar;
-	char wordChar[] = "KCATS_RAHC";
-	for(int i = 0; i < strlen(wordChar); i++) { arrayChar.push(wordChar[i]); }
-	while(!arrayChar.empty()) {
-		cout << arrayChar.pop() << " ";
+	try {
+		cout << endl << "---empty.cpp---" << endl;
+		Stack<char> arrayChar;
+		char wordChar[] = "KCATS_RAHC";
+		for(int i = 0; i < sizeof(wordChar); i++) { arrayChar.push(wordChar[i]); }
+		while(!arrayChar.empty()) {
+			cout << arrayChar.pop() << " ";
+		}
+		cout << endl << endl;
 	}
-	cout << endl << endl;
+	catch(const std::exception& e) {
+		cout << e.what() << endl;
+	}
 }
 
 int main() {
--- examples/pop.cpp
+++ examples/pop.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 
 void stackPop() {
-	cout << endl << "---pop.cpp---" << endl;
-	Stack<int> arrayInt;
-	for(int i = 0; i < 10; i++) { arrayInt.push(i); }
-	for(int j = 0; j < 6; j++) { cout << arrayInt.pop() << " "; }
-	cout << endl << endl;
+	try {
+		cout << endl << "---pop.cpp---" << endl;
+		Stack<int> arrayInt;
+		for(int i = 0; i < 10; i++) { arrayInt.push(i); }
+		for(int j = 0; j < 6; j++) { cout << arrayInt.pop() << " "; }
+		cout << endl << endl;
+	} catch(const std::exception& e) {
+		cout << e.what() << endl;
+	}
 }
 
 int main() {
