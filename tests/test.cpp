624444829249ca58c6c4022978d8eb30ba2cfa69
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-16 00:43:01 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,6 +7,16 @@ TEST_CASE("Check functions", "[stack]") {
 		Stack<int> IntArr;
 		REQUIRE(IntArr.count() == 0);
 	}
+	SECTION("Check POP")
+	{
+		Stack<int> IntArr;
+		IntArr.push(1);
+		IntArr.push(2);
+		IntArr.push(3);
+		IntArr.pop();
+		IntArr.pop();
+		REQUIRE(IntArr.count() == 1);
+	}
 	SECTION("Check EMPTY")
 	{
 		Stack<int> IntArr;
@@ -16,17 +26,15 @@ TEST_CASE("Check functions", "[stack]") {
 		IntArr.pop();
 		IntArr.pop();
 		IntArr.pop();
-		REQUIRE(IntArr.count() == 1);
-	}
-	SECTION("Check INIT")
-	{
-		Stack<int> IntArr;
 		REQUIRE(IntArr.count() == 0);
 	}
-	SECTION("Check INIT")
+	SECTION("Check PUSH")
 	{
 		Stack<int> IntArr;
-		REQUIRE(IntArr.count() == 0);
+		IntArr.push(1);
+		IntArr.push(2);
+		IntArr.push(3);
+		REQUIRE(IntArr.count() == 3);
 	}
 }
 
