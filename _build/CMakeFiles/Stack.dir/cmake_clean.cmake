a31962d2054648b9e8f21457b8cdf32fd003c20f
VladislavMashkov <Vladislav_mashkow@mail.ru>
2017-10-15 22:09:59 +0300

    added tests

---
+++ _build/CMakeFiles/Stack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stack.dir/sources/main.cpp.o"
+  "libStack.pdb"
+  "libStack.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vladislav/workspace/labs/reports/HW1_04/tests/main.cpp" "/Users/vladislav/workspace/labs/reports/HW1_04/_build/CMakeFiles/check.dir/tests/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/vladislav/workspace/labs/reports/HW1_04/_build/CMakeFiles/Stack.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
