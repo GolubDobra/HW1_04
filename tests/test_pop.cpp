7e1f3175af10f668bc7c5c8b94a9264e965dc5dd
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-16 00:21:30 +0300

    Update test_pop.cpp

--- tests/test_pop.cpp
+++ tests/test_pop.cpp
@@ -1,17 +1,16 @@
 #include "catch.hpp"
 #include <Stack.hpp>
 
-TEST_CASE("Check init", "[stack]") {
+TEST_CASE("Check pop", "[stack]") {
 	Stack<int> IntArr;
-
 		IntArr.push(1);
 		IntArr.push(2);
 		IntArr.push(3);
-    IntArr.push(4);
+    		IntArr.push(4);
 		int val = IntArr.top();
 		IntArr.pop();
 		IntArr.pop();
 
-		REQUIRE(IntArr.count() == 1);
+		REQUIRE(IntArr.count() == 2);
 		REQUIRE(val == 4);
 }
