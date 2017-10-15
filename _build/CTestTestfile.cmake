cdaffa32f919adc3f62ab3fae7eeb6c6434a9cc4
VladislavMashkov <Vladislav_mashkow@mail.ru>
2017-10-15 22:14:28 +0300

    added tests

--- _build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vladislav/workspace/labs/reports/HW1_04
-# Build directory: /Users/vladislav/workspace/labs/reports/HW1_04/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/Users/vladislav/workspace/labs/reports/HW1_04/_build/check" "-s" "-r" "compact" "--use-colour" "yes")
